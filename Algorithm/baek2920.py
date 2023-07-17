19ea4c61186e9f229f8064a49291e4b026cb14ed
GwangYoung <gospotb1607@gmail.com>
2023-07-17 19:22:00 +0900

    백준 2920 : 2023.07.17

---
+++ Algorithm/baek2920.py
@@ -0,0 +1,11 @@
+import sys
+
+number_list = list(map(int, sys.stdin.readline().split()))
+
+if sorted(number_list) == number_list:
+    print('ascending')
+elif sorted(number_list, reverse=True) == number_list:
+    print('descending')
+else:
+    print('mixed')
+
