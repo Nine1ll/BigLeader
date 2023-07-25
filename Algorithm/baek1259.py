046574886f190a089e29a39d97ab39c40a4ea640
Nine1ll <gospotb1607@gmail.com>
2023-07-25 23:47:56 +0900

    백준 1259 : 2023.07.25

---
+++ Algorithm/baek1259.py
@@ -0,0 +1,15 @@
+import sys
+
+while True:
+    number = sys.stdin.readline().replace("\n", "")
+    if not int(number):
+        break
+
+    reverse = ""
+    for i in range(len(number) - 1, 0 - 1, -1):
+        reverse += number[i]
+
+    if reverse == number:
+        print("yes")
+    else:
+        print("no")
