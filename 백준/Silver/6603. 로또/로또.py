74c098ce30fffef9378f8f800a4b42c175486296
GwangYoung <gospotb1607@gmail.com>
2023-07-29 11:30:34 +0900

    [Silver II] Title: 로또, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,12 +1,15 @@
 import sys
 
-
-def select_ele(numbers):
-    if numbers:
-        result = select_ele(numbers[:-1])
-        return result + [c + [numbers[-1]] for c in result]
-    else:
-        return [[]]
+def select_ele(lotto_list):
+    for idx in range(len(lotto_list)):
+        for jdx in range(idx + 1, len(lotto_list)):
+            for kdx in range(jdx + 1, len(lotto_list)):
+                for ldx in range(kdx + 1, len(lotto_list)):
+                    for mdx in range(ldx + 1, len(lotto_list)):
+                        for ndx in range(mdx + 1, len(lotto_list)):
+                            print(lotto_list[idx], lotto_list[jdx],
+                                  lotto_list[kdx], lotto_list[ldx],
+                                  lotto_list[mdx], lotto_list[ndx])
 
 
 while True:
@@ -17,9 +20,5 @@ while True:
     numbers = input_list[1:]
     # N 개 중에 6개를 고르는 경우의 수만큼, 그리고 그 모든 경우의 수를 출력 해야함
     # 하니씩 뽑은 다음에 재귀 함수르 넣어 주면
-    numbers = select_ele(numbers)
-    numbers.sort()
-    for lst in numbers:
-        if len(lst) == 6:
-            print(*lst)
+    select_ele(numbers)
     print()
