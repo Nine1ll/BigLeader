b0917747d2b92c743f256a4865378c72e988ba52
GwangYoung <gospotb1607@gmail.com>
2023-08-07 16:08:32 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,12 @@
+import sys
+
+l = int(sys.stdin.readline())
+
+obj_lst = {chr(i): i - 96 for i in range(97, 97 + 26)}
+string_hash = sys.stdin.readline().rstrip()
+
+ans = 0
+for idx, ele in enumerate(string_hash):
+    ans += obj_lst[ele] * 31**idx
+
+print(ans)
