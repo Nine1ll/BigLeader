98d3b947f23c341e4872aba4ea4af71605796fef
GwangYoung <gospotb1607@gmail.com>
2023-08-09 12:38:24 +0900

    [Silver II] Title: 스택 수열, Time: 4112 ms, Memory: 36696 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def stack_sequence(n, sequence):
+    stack = []
+    result = []
+    current = 1
+
+    for num in sequence:
+        while current <= num:
+            stack.append(current)
+            result.append('+')
+            current += 1
+
+        if stack[-1] == num:
+            stack.pop()
+            result.append('-')
+        else:
+            return ['NO'] 
+
+    return result
+
+
+n = int(sys.stdin.readline())
+sequence = [int(input()) for _ in range(n)]
+
+result = stack_sequence(n, sequence)
+
+for r in result:
+    print(r)
