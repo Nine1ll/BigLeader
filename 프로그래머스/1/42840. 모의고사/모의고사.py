1d40ffc18938c65ce7b1cf193878878e4f106692
Kim GwangYeong <gospotb1607@gmail.com>
2024-03-16 13:26:42 +0900

    [level 1] Title: 모의고사, Time: 3.49 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,19 @@
-import sys
-
 def solution(answers):
-    answer = []
-    scores = [0] * 3
     patterns = [[1,2,3,4,5],
-            [2,1,2,3,2,4,2,5],
-            [3,3,1,1,2,2,4,4,5,5]]
-    
+               [2,1,2,3,2,4,2,5],
+               [3,3,1,1,2,2,4,4,5,5]]
+    scores = [0]*3
     for i, pattern in enumerate(patterns):
-        for j, number in enumerate(answers):
-            if number == pattern[j % len(pattern)]:
+        for j, answer in enumerate(answers):
+            if answer == pattern[j % len(pattern)]:
                 scores[i] += 1
-    
-    max_score = max(scores)
 
-    for i, score in enumerate(scores):
-        if score == max_score:
-            answer.append(i+1)
+    max_score = max(scores)
+    answer = []
+    
+    for k, socre in enumerate(scores):
+        if socre == max_score:
+            answer.append(k+1)
+    answer.sort()
 
     return answer
-
-answer = list(map(int, sys.stdin.readline().split()))
-print(solution(answer))
