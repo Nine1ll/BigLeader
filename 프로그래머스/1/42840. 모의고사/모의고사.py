68c7f3ec97b59ec31600165d4eb96e3e045f9a36
Kim GwangYeong <gospotb1607@gmail.com>
2024-04-20 11:19:20 +0900

    [level 1] Title: 모의고사, Time: 3.72 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,18 @@
 def solution(answers):
-    patterns = [[1,2,3,4,5],
-               [2,1,2,3,2,4,2,5],
-               [3,3,1,1,2,2,4,4,5,5]]
-    scores = [0]*3
-    for i, pattern in enumerate(patterns):
-        for j, answer in enumerate(answers):
-            if answer == pattern[j % len(pattern)]:
-                scores[i] += 1
-
-    max_score = max(scores)
     answer = []
+    count = [0]*3
+    patterns = [[1,2,3,4,5],
+              [2,1,2,3,2,4,2,5],
+              [3,3,1,1,2,2,4,4,5,5]]
+    for num, pattern in enumerate(patterns):
+        for i in range(len(answers)):
+            pattern_index = i % len(pattern)
+            if answers[i] == pattern[pattern_index]:
+                count[num] += 1
     
-    for k, socre in enumerate(scores):
-        if socre == max_score:
-            answer.append(k+1)
-    answer.sort()
-
+    max_score = max(count)
+    for i in range(len(count)):
+        if max_score == count[i]:
+            answer.append(i+1)
+    
     return answer
